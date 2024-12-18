9ed6d4a8b2cb1687a0d848dfa1e1ed58ed82d067
Jogin200 <joginsimon14@gmail.com>
2024-12-18 16:04:22 +0530

    push visitor

--- app/src/main/java/com/example/visitorapp/MainActivity.java
+++ app/src/main/java/com/example/visitorapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.visitorapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +11,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e1,e2,e3,e4;
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        e1=(EditText) findViewById(R.id.usr);
+        e2=(EditText) findViewById(R.id.lnm);
+        e3=(EditText) findViewById(R.id.whm);
+        e4=(EditText) findViewById(R.id.whm);
+        b1=(Button) findViewById(R.id.lgn);
+
     }
 }
